973861666c911be96933e9c65a0bdf3d9c3b37fa
Henrique da Silva Cardoso <henrique.silva@warren.com.br>
2023-01-04 19:47:52 -0300

    feat: saved the file

--- Everest03.NET/Services/IService.cs
+++ Everest03.NET/Services/IService.cs
@@ -1,6 +1,13 @@
-﻿namespace Everest03.NET.Services
+﻿using System.Collections.Generic;
+
+namespace Everest03.NET.Services
 {
     public interface IService
     {
+        void DeleteCustomer(long id);
+        Customer GetCustomerById(long Id);
+        List<Customer> GetCustomers();
+        void SetCustomer(Customer customer);
+        void UpdateCustomer(long Id, Customer customer);
     }
 }
