9403503b2c62a3dc045b07958c12c3f29d4d3b3f
Henrique da Silva Cardoso <113135850+HenriqueSilvaWarren@users.noreply.github.com>
2023-01-05 09:09:06 -0300

    Update Everest03.NET/Validators/CustomersValidator.cs
    Co-authored-by: Gregory Zimmer <113135420+gregoryzimmerwarren@users.noreply.github.com>

--- Everest03.NET/Validators/CustomersValidator.cs
+++ Everest03.NET/Validators/CustomersValidator.cs
@@ -9,19 +9,53 @@ namespace Everest03.NET.Validators
     {
         public CustomersValidator()
         {
-            RuleFor(customer => customer.FullName).NotEmpty().MinimumLength(8);
-            RuleFor(customer => customer.Email).NotEmpty().EmailAddress();
-            RuleFor(customer => customer).Must(customer => customer.Email == customer.EmailConfirmation).WithMessage("Email e EmailConfirmation precisam ser iguais");
-            RuleFor(customer => customer.Cpf).NotEmpty().IsValidCPF();
-            RuleFor(customer => customer.Cellphone).NotEmpty().Matches(@"\([0-9]{2}\)[0-9]{5}-[0-9]{4}");
-            RuleFor(customer => customer.DateOfBirth).NotEmpty().LessThanOrEqualTo(DateTime.Now.AddYears(-18));
-            RuleFor(customer => customer.EmailSms).NotEmpty();
-            RuleFor(customer => customer.Whatsapp).NotEmpty();
-            RuleFor(customer => customer.Country).NotEmpty().MinimumLength(4);
-            RuleFor(customer => customer.City).NotEmpty();
-            RuleFor(customer => customer.PostalCode).NotEmpty().Matches(@"[0-9]{5}-[0-9]{3}");
-            RuleFor(customer => customer.Address).NotEmpty().MinimumLength(10);
-            RuleFor(customer => customer.Number).NotNull();
+            RuleFor(customer => customer.FullName)
+            .NotEmpty()
+            .MinimumLength(8);
+            
+            RuleFor(customer => customer.Email)
+            .NotEmpty()
+            .EmailAddress();
+            
+            RuleFor(customer => customer)
+            .Must(customer => customer.Email == customer.EmailConfirmation)
+            .WithMessage("Email e EmailConfirmation precisam ser iguais");
+            
+            RuleFor(customer => customer.Cpf)
+            .NotEmpty()
+            .IsValidCPF();
+            
+            RuleFor(customer => customer.Cellphone)
+            .NotEmpty()
+            .Matches(@"\([0-9]{2}\)[0-9]{5}-[0-9]{4}");
+            
+            RuleFor(customer => customer.DateOfBirth)
+            .NotEmpty()
+            .LessThanOrEqualTo(DateTime.Now.AddYears(-18));
+            
+            RuleFor(customer => customer.EmailSms)
+            .NotEmpty();
+            
+            RuleFor(customer => customer.Whatsapp)
+            .NotEmpty();
+            
+            RuleFor(customer => customer.Country)
+            .NotEmpty()
+            .MinimumLength(4);
+            
+            RuleFor(customer => customer.City)
+            .NotEmpty();
+            
+            RuleFor(customer => customer.PostalCode)
+            .NotEmpty()
+            .Matches(@"[0-9]{5}-[0-9]{3}");
+            
+            RuleFor(customer => customer.Address)
+            .NotEmpty()
+            .MinimumLength(10);
+            
+            RuleFor(customer => customer.Number)
+            .NotEmpty();
         }
     }
 }
